dcc964fd3d16bf135a27742d2d24c8676f01925f
Alexander <alex.byvaltsev@mail.ru>
2022-07-11 17:34:17 +0300

    fix temp color

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -3,6 +3,7 @@ import s from "./Weather.module.scss";
 import { Outlet } from "react-router-dom";
 import { Location } from "../../features/location/Location";
 
+
 export const Weather: FC = () => {
   return (
     <div className={s.container}>
